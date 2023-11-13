d8a1d691dcfbabe8ad89b68239cfcd70fa6186fa
Juni188 <134086025+Juni188@users.noreply.github.com>
2023-11-13 18:07:33 +0100

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='math_quiz',
+    version='0.1.1',
+    author='DSSS',
+    
+    packages=find_packages(),
+    install_requires=["numpy"],
+)
